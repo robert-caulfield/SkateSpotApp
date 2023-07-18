d04bfbece520314535d306e94e74fe813c58a427
Robert Caulfield <113054389+robert-caulfield@users.noreply.github.com>
2023-07-18 02:49:31 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/skatespotapp/MainActivity.kt
+++ app/src/main/java/com/example/skatespotapp/MainActivity.kt
@@ -11,30 +11,7 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
-    /*
 
-
-    Firebase Login:
-    hwk4@example.com
-    abcdef
-
-    my email: robert.w.caulfield@gmail.com
-
-    About
-    This is an app that allows users to share skateboarding spots.
-
-    After signing in you are able to scroll through a list of uploaded spots.
-    You can view a description of these spots and view the location in a mapview
-    upon clicking the recycler view items.
-
-    Users has the option of adding their own spots, using the mapview to assign
-    the location, indicated by a marker. If location services are enabled, the user
-    is able to zoom in on their current location by clicking the icon in the top
-    right of the mapview.
-
-    The settings menu allows users to change some of the features of the mapview.
-
-     */
 
 
     //Login Views
